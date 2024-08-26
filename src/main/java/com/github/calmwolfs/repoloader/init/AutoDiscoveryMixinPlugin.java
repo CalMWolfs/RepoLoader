81e1783f09bf2beaba5df4e342b5dddb5d3a89e5
GitHub Action <action@github.com>
2024-08-26 01:31:19 +0000

    Initialize template repository proper
    Set package name to com.github.calmwolfs.repoloader
    Set project name to RepoLoader
    Set modid to repoloader

--- src/main/java/com/github/calmwolfs/repoloader/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/calmwolfs/repoloader/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.calmwolfs.repoloader.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
